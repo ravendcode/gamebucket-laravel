85d5c9d36dbe733fd76736e1bc45c91657dd66f2
ravendcode <ravendcode@gmail.com>
2019-04-13 14:47:40 +0300

    Fix php version

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Game;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 use App\Services\Zipper;
 
